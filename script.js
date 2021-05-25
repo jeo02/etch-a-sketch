5983e75fffdd3a5f8cca199975ba63c5fb1bea5b
jeo02 <juan.ospina@stonybrook.edu>
2021-05-25 17:06:54 -0400

    change grid size
    changes the grid size while mainting the dimensions of 500X500

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
 let container = document.querySelector("#container");
 let dimension = 16;
 let gridSize = 500/dimension + "px";
-let gridDimensions = "";
+window.onload = resize();
 
-//making the dimensions of the grid fit to 500x500 pixels 
-for(let i = 0; i <= dimension; i++)
-{
-    gridDimensions += gridSize + " ";
-}
-
-container.style.gridTemplateRows = gridDimensions;
-container.style.gridTemplateColumns = gridDimensions;
 
 //16x16 div elements in the grid
-for(let i = 1; i <= dimension; i++){
-    for(let j = 1; j <= dimension; j++){
-        let content = document.createElement('div');
-        content.setAttribute("class", "box");
-        container.appendChild(content);
-        content.style.gridColumnStart = j;
-        content.style.gridRowStart = i;
+function resize(){
+    //clearing elements from container
+    container.innerHTML = '';
+    //making the dimensions of the grid fit to 500x500 pixels 
+    let gridDimensions = "";
+    for(let i = 0; i < dimension; i++)
+    {
+        gridDimensions += gridSize + " ";
     }
+
+    container.style.gridTemplateRows = gridDimensions;
+    container.style.gridTemplateColumns = gridDimensions;
+
+    for(let i = 1; i <= dimension; i++){
+        for(let j = 1; j <= dimension; j++){
+            let content = document.createElement('div');
+            content.setAttribute("class", "box");
+            container.appendChild(content);
+            content.style.gridColumnStart = j;
+            content.style.gridRowStart = i;
+        }
+    }
+
+    //grid elements change color on hover
+    let squares = document.querySelectorAll('.box');
+    let colors = ['blue', 'red', 'yellow', 'purple', 'peach', 'orange', 'turquoise', 'pink', 'green', 'lime', 'brown', 'teal'];
+    squares.forEach(square =>{
+        square.addEventListener('mouseover', function(e){
+            let num = Math.floor(Math.random()*12);
+            e.target.style.background = colors[num];
+        })
+    })
 }
 
-//grid elements change color on hover
-let squares = document.querySelectorAll('.box');
-let colors = ['blue', 'red', 'yellow', 'purple', 'peach', 'orange', 'turquoise', 'pink', 'green', 'lime', 'brown', 'teal'];
-squares.forEach(square =>{
-    square.addEventListener('mouseover', function(e){
-        let num = Math.floor(Math.random()*12);
-        console.log(num);
-        e.target.style.background = colors[num];
-    })
+
+
+
+let button = document.querySelector('#button');
+button.addEventListener('click', function(){
+    let valid = false;
+    while(!valid){
+        let userInput = prompt("Please eneter the new grid size.");
+        if(userInput != null && userInput >0 && userInput <= 64){
+            dimension = userInput;
+            gridSize = 500/dimension + "px";
+            resize();
+            valid = true;
+        }
+        else{
+            alert("Grid size must be a number between 1 and 64");
+        }
+    }
+    
 })
 
